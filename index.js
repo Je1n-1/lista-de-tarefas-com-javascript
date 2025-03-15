c6756616a898a4b9812d3c5f87890a368448d473
Jean_Land <Jeanmiranda1255@gmail.com>
2025-03-15 08:03:39 -0300

    javascript código base

---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let contador = 0;
+    const listaDeCompras = document.getElementById('lista-de-compras');
+    const inputItem = document.getElementById('input-item');
+    const botaoAdicionar = document.getElementById('adicionar-item');
+
+    botaoAdicionar.addEventListener('click', (evento) => {
+        if (inputItem.value.trim() === '') {
+            alert('Digite um item para adicionar');
+            return;
+        }
+
+        const itemDaLista = document.createElement('li');
+        const containerItemDaLista = document.createElement('div');
+        containerItemDaLista.classList.add('lista-item-container');
+
+        const inputCheckbox = document.createElement('input');
+        inputCheckbox.type = 'checkbox';
+        inputCheckbox.id = `checkbox-${contador++}`; // Evita conflitos de ID
+
+        const nomeItem = document.createElement('p');
+        nomeItem.innerText = inputItem.value;
+
+        containerItemDaLista.appendChild(inputCheckbox);
+        containerItemDaLista.appendChild(nomeItem);
+
+        itemDaLista.appendChild(containerItemDaLista);
+        listaDeCompras.appendChild(itemDaLista);
+
+        inputItem.value = ''; // Limpa o campo após adicionar
+    });
+});
